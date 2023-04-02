b7151612d3077f0b8e40130d6bfcc3564097d68e
Jaspreet <jaspreetkhosa.003@gmail.com>
2023-04-02 11:37:09 -0600

    Model training files added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,8 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransform
 from src.components.data_transformation import DataTransformconfig
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionconfig:
@@ -25,11 +27,14 @@ class DataIngestion:
         logging.info("Enter the Data ingestion methods")
         try:
             df = pd.read_csv("notebook\data\StudentsPerformance.csv")
+            
             logging.info("Read the dataset as a DataFrame")
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok= True)
             df.to_csv(self.ingestion_config.raw_data_path, index= False, header= True)
+           
             logging.info("Train test Split ")
+           
             train_set, test_set = train_test_split(df, test_size= 0.2, random_state= 42)
             train_set.to_csv(self.ingestion_config.train_data_path, index = False, header= True)
             test_set.to_csv(self.ingestion_config.test_data_path, index = False, header= True)
@@ -49,4 +54,7 @@ if __name__=='__main__':
     obj = DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
     data_transform = DataTransform()
-    data_transform.initiate_data_transform(train_data, test_data)
+    train_arr, test_arr,_ = data_transform.initiate_data_transform(train_data, test_data)
+
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
